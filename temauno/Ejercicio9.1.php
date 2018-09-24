1b1125d784aa349e74769991d5e5180cfdca3d1e
daniel5213 <daniel.alvarez.santiago@gmail.com>
2018-09-24 11:46:13 +0200

    quinto comit

---
+++ temauno/Ejercicio9.1.php
@@ -0,0 +1,17 @@
+<?php
+include 'Ejercicio9.php';
+$number =0;
+$format= ' ' ;
+echo "Introduce el numero:";
+fscanf(STDIN, "%d\n",$number);
+
+echo "Da el formato:";
+fscanf(STDIN, "%s\n",$format);
+
+/*Strtoslower: es una funcion para cambiar la cadena caracteres a minusculas*/
+$format = strtolower ($format);
+
+for ($i=0;$i<$number;$i++){
+    echo $array[$format][$i]." ";
+}
+?>
---
+++ temauno/Ejercicio9.php
@@ -0,0 +1,8 @@
+<?php
+
+$array =[
+    "nombre"=>["uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","ciez"],
+    "romano" => ["I","I","III","IV","V","VI","VII","VIII","IX","X"]
+];
+
+?>
