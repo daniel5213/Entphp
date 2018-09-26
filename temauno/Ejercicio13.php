34c34e34e62b913e7208f04f0224a02270f63f97
daniel5213 <daniel.alvarez.santiago@gmail.com>
2018-09-26 10:17:10 +0200

    Ejercicio13: Hecho

---
+++ temauno/Ejercicio13.php
@@ -0,0 +1,11 @@
+<?php
+$sting = "El presidente es muy tonto y es un analfabeto por no saber hablar";
+$lista = $sting;
+echo "Texto sin remplazar \n$sting\n";
+$prohibida =["tonto","analfabeto"];
+echo "texto remplazado\n";
+foreach ($prohibida as $prohibida){
+$sting= str_replace($prohibida, "******", $sting);
+}
+echo $sting;
+?>
