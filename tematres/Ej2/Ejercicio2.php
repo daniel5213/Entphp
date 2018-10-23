6ccc2ef9adecfb69b7a491591441c302054d0911
daniel5213 <daniel.alvarez.santiago@gmail.com>
2018-10-23 11:59:13 +0200

    Ejercicio2: echo

--- tematres/Ej2/Ejercicio2.php
+++ tematres/Ej2/Ejercicio2.php
@@ -1,9 +1,9 @@
 <?php
     //si existe el uid del coockie es true, si no existe es false y devuelve 0;
-    $n_visitas = isset ( $_COOKIE ['UID'] ) ? $_COOKIE ['UID'] : 0;
+    $visitas = isset ( $_COOKIE ['UID'] ) ? $_COOKIE ['UID'] : 0;
     //primeravez es nvisitas a 0
-    $primeraVez = ($n_visitas == 0);
-    echo "Numero de visitas $n_visitas";
+    $primeraVez = ($visitas == 0);
+    echo "Numero de visitas $visitas";
     /*Si primera vez es distinta que 0 
      * añadimos a la uid el numero 1.
      * Si no es disinto que 0
@@ -12,9 +12,9 @@
         setcookie ( "UID", 1 );
         echo "<h1>Bienvenido</h1>";
     } else {
-        setcookie("UID",$n_visitas + 1);
-        echo "Numero de visitas en el if $n_visitas";
-        echo "<h2>Bienvenido eres el visitante $n_visitas en esta pagina</h2>";
+        setcookie("UID",$visitas + 1);
+        echo "Numero de visitas en el if $visitas";
+        echo "<h2>Bienvenido eres el visitante $visitas en esta pagina</h2>";
      
         
         
