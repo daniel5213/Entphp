dc605fa82d743565d61d27686a065bb61317c395
daniel5213 <daniel.alvarez.santiago@gmail.com>
2018-10-24 00:01:42 +0200

    Ejercicio 9:Plantillas terminadas

---
+++ tematres/Ej9/escribir.php
@@ -0,0 +1,7 @@
+<form>
+<label id="de">De:</label>
+<input type="text" name="" value=""/><br/>
+<label id="de">De:</label>
+<input type="text" name="" value=""/><br/>
+<textarea rows="120px" cols="90px">Escribe el contenido del mensaje</textarea>
+</form>
---
+++ tematres/Ej9/leer.php
@@ -0,0 +1,11 @@
+<h2>Usuario actual</h2>
+
+<form>
+Lista de mensajes de 
+
+<table>
+<tr>
+<th></th>
+</tr></table>
+</form>
+<a href="home.php">Volver a lista de usuarios</a>
