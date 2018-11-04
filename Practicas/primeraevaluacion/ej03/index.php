24621d522891527769700da52acc8f56a5fbdda1
daniel5213 <daniel.alvarez.santiago@gmail.com>
2018-11-04 21:10:39 +0100

    Ejercicio 3: Completada

--- Practicas/primeraevaluacion/ej03/index.php
+++ Practicas/primeraevaluacion/ej03/index.php
@@ -1,36 +1,22 @@
-<?php
-
-
-?>
-
 <script>
-
 function pelicula() {
-
-	pelicula = document.getElementById('peli').value;
 	conexion = new XMLHttpRequest();
 	conexion.open("POST","ajax.php",true);
 	conexion.setRequestHeader('X-Requested-With','XMLHttpRequest');
-	
-	conexion.send("peli="+pelicula);
-
+	conexion.send();
 	conexion.onreadystatechange = function() {
 		if (conexion.readyState == 4 && conexion.status==200) {
 			respuesta = conexion.responseText;
-			
-				
 				document.getElementById("peli").value = respuesta;
-				
-			
-			
 		}
 	}
-	
 }
 </script>
 
 
-<button onclick="pelicula()">Peli favorita</button><input type="text" id="peli" value="titanic"/><br/>
-<div id="panel"/>
+<button onclick="pelicula()">Peli favorita</button>
+<input type="text" id="peli" readonly="readonly" />
+<br />
+<div id="panel" />
 
 
